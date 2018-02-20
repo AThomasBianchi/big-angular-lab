4c5e573589e9146cfa0ba5687aad9f40751c1ed6
Tom Bianchi <atbianchi@gmail.com>
2018-02-20 14:36:26 -0500

    Factor Service

---
+++ factorsService.js
@@ -0,0 +1,80 @@
+(function() {
+  var factorNumber;
+  function FactorService() {
+    return {
+      primeFactor: PrimeFactor,
+      isPrime: isPrime,
+      setNumber: setNumber
+      getFactors: getFactors
+    }
+  }
+
+  function isPrime(num) {
+    for (var i=2; i<num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function primeFactor(bigNum) {
+    var pfs = [];
+    var i = 2;
+    while (i <= bigNum) {
+      if ( !(bigNum % i === 0) || !(isPrime(i)) ) {
+        i++;
+      } else {
+        pfs.push(i);
+        bigNum = bigNum / i;
+        i = 2;
+      }
+    }
+    return pfs;
+    console.log("Prime Factorial List:" pfs)
+  }
+
+  function setNumber(inputNumber) {
+    factorNumber = inputNumber;
+    console.log(factorNumber);
+  }
+
+  function getFactors() {
+    return primeFactor(factorNumber);
+  }
+
+  angular
+    .module("app")
+    .Factory("FactorService", FactorService);
+
+}());
+
+
+// function isPrime(num) {
+//   for (var i=2; i<num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+//
+// function primeFactor(bigNum) {
+//   var pfs = [];
+//   var i = 2;
+//   while (i <= bigNum) {
+//     if ( !(bigNum % i === 0) || !(isPrime(i)) ) {
+//       i++;
+//       continue
+//     // } else if (!isPrime(i)) {
+//     //    i++;
+//     //    continue
+//     } else {
+//       pfs.push(i);
+//       bigNum = bigNum / i;
+//       i = 2;
+//     }
+//   }
+//   return pfs;
+// }
