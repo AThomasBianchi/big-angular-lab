dc67b758faad374fc726c74b2953f7776c4179f9
George Morris <gmorris1242@gmail.com>
2018-02-20 15:35:21 -0500

    facts service and components working

---
+++ factsComponents.js
@@ -0,0 +1,23 @@
+(function() {
+  var factsComponent = {
+    template: `
+              <div>
+              <p>{{ $ctrl.num }}</p>
+
+              <p>{{ $ctrl.facts.text }}</p>
+
+              </div>
+
+    `,
+    controller: function(factsService){
+      var $ctrl = this
+      $ctrl.num = 45;
+      factsService.getApi($ctrl.num).then(function(facts) {
+          $ctrl.facts = facts;
+
+      });
+    }
+  }
+    angular.module("app")
+      .component("factsComponent", factsComponent)
+}());
---
+++ factsService.js
@@ -0,0 +1,29 @@
+(function() {
+  function factsService($http){
+    return {
+      getApi: getApi
+    };
+
+    function getApi(number){
+      console.log(number);
+      var num = number;
+      return $http({
+        method: "GET",
+        url: "https://numbersapi.p.mashape.com/" + num + "/trivia?fragment=true&json=true&notfound=floor",
+        headers: {
+        "X-Mashape-Key": 'd3g8GAFEaDmsh1N8s8WTrwCmn6Dpp1mZEshjsn941cD5ZSO6nO',
+      	"Accept": "application/json"
+        }
+}).then(function(response){
+        console.log(response.data);
+        return response.data;
+      })
+    };
+
+  }
+  angular.module("app")
+    .factory("factsService",factsService)
+}());
+
+
+"https://numbersapi.p.mashape.com/42/trivia?fragment=true&json=true&notfound=floor"
