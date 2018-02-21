d3de6e567da0eae74d44313728de7421ea2391bb
Tom Bianchi <atbianchi@gmail.com>
2018-02-21 09:47:24 -0500

    ng-view is magic

--- formComponent.js
+++ formComponent.js
@@ -6,14 +6,29 @@
       <input type="number" ng-model="$ctrl.number">
       <button type="submit">Enter the number</button>
     </form>
+    <div ng-show="$ctrl.truthy">
+      <h2> Your number is: </h2>
+      <p> {{ $ctrl.number }} </p>
+      <nav>
+        <a href="#!/fact">Fact</a>
+        <a href="#!/factor">Factors</a>
+        <a href="#" ng-click="$ctrl.reset()">Reset</a>
+      </nav>
+
+    </div>
     `,
     controller: function(FactorService, factsService) {
       var $ctrl = this;
+      $ctrl.truthy = false;
       $ctrl.number;
       $ctrl.setNumberTwice = function() {
-        console.log($ctrl.number);
         FactorService.setNumber($ctrl.number);
         factsService.setNumber($ctrl.number);
+        $ctrl.truthy = true;
+      }
+      $ctrl.reset = function() {
+        $ctrl.truthy = false;
+        $ctrl.number = "";
       }
     }
   }
